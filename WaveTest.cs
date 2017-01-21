1f8ff96b9837992bca59e82fe2837f40b8541a9b
keastin2814 <keastin2814@gmail.com>
2017-01-21 00:45:16 -0600

    Add files via upload

---
+++ WaveTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class WaveTest : MonoBehaviour
+{
+    public float waveLth;
+
+    RaycastHit ray;
+    RaycastHit hit;
+    Vector3 start, waveMvnt = new Vector3(1,0,0);
+    float theta;
+
+    public struct RayWave
+    {
+        public Vector3 start;
+        public float length;
+
+        public RayWave(Vector3 spwnPnt, float lgth)
+        {
+            this.length = lgth;
+            this.start = spwnPnt;
+        }
+    }
+	
+	void Update ()
+    {
+        
+            RayWave smallray = new RayWave(start, waveLth);
+            waveMvnt = new Vector3(0,Mathf.Sin(theta),0);
+            start += new Vector3(.1f, 0f,0);
+            theta += Mathf.Sin(.01f);
+            //if (Physics.Raycast(start,waveMvnt,waveLth))
+                //print("The ray " );
+            Debug.DrawRay(start,waveMvnt, Color.green,waveLth);
+        
+    }
+}
